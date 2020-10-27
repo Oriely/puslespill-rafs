38e085da905132f8d5d575d70a2229d221505a91
Oriely <orielygaming@protonmail.com>
2020-10-27 14:21:55 +0000

    pictures

--- assets/js/mainFunctions.js
+++ assets/js/mainFunctions.js
@@ -3,18 +3,16 @@
 // generate divs that are going to contain the finished puzzle
 function genPuzzleCanvas() {
 
-    const container = document.createElement('div');
+    const container = document.createElement('canvas');
     container.setAttribute('id', 'container');
+
     let piece =  document.createElement('span');
     let amountOfPieces = 100;
     let i;
 
     document.body.appendChild(container);
 
-    for (i = 0; i < amountOfPieces; i++) {
-        container.innerHTML += '<span id="canvas-piece">10</span>';
-
-    }
+    
         
 
 
