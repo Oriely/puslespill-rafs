6d0366c527ea7e89d16c78cb34b09638553a4257
Oriely <orielygaming@protonmail.com>
2020-10-27 12:53:20 +0000

    adding function that makes the puzzle grid

--- assets/js/mainFunctions.js
+++ assets/js/mainFunctions.js
@@ -3,7 +3,24 @@
 // generate divs that are going to contain the finished puzzle
 function genPuzzleCanvas() {
 
+    const container = document.createElement('div');
+    container.setAttribute('id', 'container');
+    let piece =  document.createElement('span');
+    let amountOfPieces = 100;
+    let i;
+
+    document.body.appendChild(container);
+
+    for (i = 0; i < amountOfPieces; i++) {
+        container.innerHTML += '<span id="canvas-piece">10</span>';
+
+    }
+        
+
+
 }
 
+genPuzzleCanvas();
 
 //
+    
