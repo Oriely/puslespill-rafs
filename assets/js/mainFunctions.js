2e8979cd0942acf3e51287069fecfc9b6dc6c4c3
Oriely <orielygaming@protonmail.com>
2020-10-31 01:45:29 +0000

    lsadfasd

--- assets/js/mainFunctions.js
+++ assets/js/mainFunctions.js
@@ -1,10 +1,12 @@
-
 let _numOfPieces = 399;
-let piecesImg = [];
+let _puzzlePieces =  [];
+let _puzzlePiecesScrambled = []
+let _puzzlePiecesAnswer = [];
+
 
 // div
-
 let _injectDiv = 'wrapper';
+
 
 // create elements based on information in paramaters and append them to the "wrapper"/"container" div
 function createElemWithAttr(appendTo, elem, attr, value) {
@@ -20,14 +22,33 @@ function createElemWithAttr(appendTo, elem, attr, value) {
     } return;
 }
 
+
 function randomizeArray(arr) {
     let array = arr;
     array.sort(() => Math.random() - 0.5);
     return array;
 }
 
-function generategrid() {
+function generatePuzzlePieces() {
+    // declaring variables
+    let i;
+    let _leftSide = document.getElementById('puzzle-left');
+    let _rightSide = document.getElementById('puzzle-right');
+    // push img elements with appropriate name to array.
+    for (i = 0; i <= _numOfPieces; i++){
+        _puzzlePieces.push('<img class="puzzleBox" id="b' + [i] + '" ondragstart="drag(event)" draggable="true" src="assets/images/puzzleparts/img (' + [i] + ').png">');
+        _puzzlePiecesScrambled.push('<img class="puzzleBox" id="b' + [i] + '" ondragstart="drag(event)" draggable="true" src="assets/images/puzzleparts/img (' + [i] + ').png">');
+    }
+    
+    // randomize the array with img elements and add img elements to "container-right"
+    list = randomizeArray(_puzzlePiecesScrambled);
+    for (i = 0; i <= _numOfPieces; i++) {
+        _rightSide.innerHTML += _puzzlePiecesScrambled[i];
+    }
 
+}
+
+function createLayout() {
     createElemWithAttr(_injectDiv, 'div', 'id', "inner-left");
 
     createElemWithAttr(_injectDiv, 'div', 'id', "inner-right");
@@ -36,44 +57,51 @@ function generategrid() {
 
     createElemWithAttr("inner-right", 'div', 'id', "puzzle-right");
     
-    createElemWithAttr('wrapper', 'button', 'id', 'knappers');
+    createElemWithAttr('wrapper', 'button', 'id', 'cheat');
 
-}
-generategrid();
-
-function generatePuzzlePieces() {
-    // declaring variables
-    let i;
-
+    createElemWithAttr('wrapper', 'button', 'id', 'generatePuzzle');
+    
     let _leftSide = document.getElementById('puzzle-left');
-    let _rightSide = document.getElementById('puzzle-right');
-
-    // push img elements with appropriate name to array.
-    for (i = 0; i <= _numOfPieces; i++){
-        piecesImg.push('<img class="puzzleBox" id="b' + [i] + '" ondragstart="drag(event)" draggable="true" src="assets/images/puzzleparts/img (' + [i] + ').png">');
-    }
-
-
+    document.getElementById('generatePuzzle').innerHTML = 'Generate Puzzle';
     // make the grid that you place the puzzle pieces(img elements) into to make a completed puzzle
     for(i = 0; i <= _numOfPieces; i++) {
-        _leftSide.innerHTML += '<span id="g' + [i] + '"  ondrop="drop(event)" ondragover="allowDrop(event)"></span>';
-    }
-    
-    // randomize the array with img elements and add img elements to "container-right"
-    list = randomizeArray(piecesImg);
-    for (i = 0; i <= _numOfPieces; i++) {
-        _rightSide.innerHTML += piecesImg[i];
+        _leftSide.innerHTML += '</span>';
     }
 
 }
-let knappers = document.getElementById('knappers');
 
-knappers.addEventListener('onclick', function(){
-    document.getElementById('puzzle-left').innerHTML = '';
-    for (i = 0; i <= _numOfPieces; i++) {
-        document.getElementById('puzzle-left').innerHTML += piecesImg[i];
+
+function checkAnswer() {
+    let i;
+    let imgs = document.querySelector('.puzzleBox');
+
+
+    for(i = 1; i < _numOfPieces; i++) {
+        _puzzlePiecesAnswer.push(imgs[i].innerHTML);
     }
-});
+
+    if(_puzzlePiecesAnswer === _puzzlePieces) {
+        console.log('help');
+    }
+    else {
+        console.log('no');
+    }
+}
+
+
+function knappersr()  {
+    let i;
+    let piecesImgs = [];
+    console.log('22');
+    document.getElementById('puzzle-left').innerHTML = '';
+    for (i = 0; i <= _numOfPieces; i++){
+        piecesImgs.push('<span class="puzzlebBox-left id="g' + [i] + '"  ondrop="drop(event)" ondragover="allowDrop(event)"><img class="puzzleBox" id="b' + [i] + '" ondragstart="drag(event)" draggable="true" src="assets/images/puzzleparts/img (' + [i] + ').png"></span>');
+    }
+    for (i = 0; i <= _numOfPieces; i++) {
+
+        document.getElementById('puzzle-left').innerHTML += piecesImgs[i];
+    }
+}
 
 //Drag + drop secion
 function allowDrop(ev) {
@@ -82,17 +110,27 @@ function allowDrop(ev) {
 
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-  }
+}
 
-  function drop(ev) {
+function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-  }
-// load functions
-window.onload = function() {
+}
 
-    generatePuzzlePieces();
+// load functions
+createLayout();
+
+window.onload = function() {
+    document.getElementById('generatePuzzle').addEventListener('click', function() {
+        generatePuzzlePieces();
+    });
+    document.getElementById('cheat').addEventListener('click', function() {
+        knappersr();
+
+    });
+    
+    
 }
 
 
